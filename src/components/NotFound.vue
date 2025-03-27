55bebc43d02e32c54d4e9e79ccd52009c27aebb5
ai862 <202080125+ai862@users.noreply.github.com>
2025-03-27 10:35:45 +0800

    Create NotFound.vue

---
+++ src/components/NotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <p>抱歉，您访问的页面不存在</p>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+}
+
+h1 {
+  font-size: 6rem;
+  margin-bottom: 1rem;
+  color: #666;
+}
+
+p {
+  font-size: 1.5rem;
+  color: #999;
+}
+</style>
